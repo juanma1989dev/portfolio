7b03dd708456ffc1bee3b6dbc91b262a109113ac
Juan Manuel GR <jmgr198810@gmail.com>
2025-11-21 19:29:48 +0000

    feat: se crean los componentes para  los iconos de github y linkedin

---
+++ src/const.js
@@ -0,0 +1,5 @@
+const URL_GITHUB = "https://github.com/juanma1989dev";
+const URL_LINKEDIN =
+  "https://www.linkedin.com/in/juan-manuel-guzman-rodriguez-211a90135";
+
+export { URL_GITHUB, URL_LINKEDIN };

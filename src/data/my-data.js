63b8863ffa56ff026d80641a7ca8106e9278e9a9
Juan Manuel GR <jmgr198810@gmail.com>
2025-12-11 00:08:05 +0000

    El SEO de la pagina se centraliza en un archivo

---
+++ src/data/my-data.js
@@ -0,0 +1,9 @@
+const NAME = "Juan Manuel Guzmán Rodríguez";
+const ROLE = "Full Stack Developer";
+const WEB_DESCRIPTION =
+  "Soy desarrollador de software con más de 9 años de experiencia creando soluciones tecnológicas que resuelven problemas reales. Me especializo en aplicaciones web robustas y escalables utilizando PHP, JavaScript y bases de datos como MySQL y Oracle.";
+const WEBSITE_URL = "https://juan.hublava.com";
+
+const NAME_ROLE = `${NAME} - ${ROLE}`;
+
+export { NAME, ROLE, WEB_DESCRIPTION, WEBSITE_URL, NAME_ROLE };
